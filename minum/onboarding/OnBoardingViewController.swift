ca106e3b09418ee224a1d97bf8160b15665268c8
Ihwan <mynameisihwan@gmail.com>
2020-05-12 23:44:46 +0700

    add onboarding

---
+++ minum/onboarding/OnBoardingViewController.swift
@@ -0,0 +1,39 @@
+//
+//  OnBoardingViewController.swift
+//  minum
+//
+//  Created by Ihwan ID on 12/05/20.
+//  Copyright © 2020 Ihwan ID. All rights reserved.
+//
+
+import UIKit
+
+class OnBoardingViewController: UIViewController {
+
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+       
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateInitialViewController() as? UITabBarController ?? UITabBarController()
+            vc.modalPresentationStyle = .fullScreen
+            present(vc, animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

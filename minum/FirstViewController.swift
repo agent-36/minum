9bd3b9028e859bd8afa9d050a58bf1c02f5dcb48
Ihwan <mynameisihwan@gmail.com>
2020-05-12 17:42:42 +0700

    Initial Commit

---
+++ minum/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  minum
+//
+//  Created by Ihwan ID on 12/05/20.
+//  Copyright © 2020 Ihwan ID. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ minum/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  minum
+//
+//  Created by Ihwan ID on 12/05/20.
+//  Copyright © 2020 Ihwan ID. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

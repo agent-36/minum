ec68d2f7cd6abb5ea7c61169b58e3acda9f46248
Ihwan <mynameisihwan@gmail.com>
2020-05-13 14:58:50 +0700

    create water screen

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'minum' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for minum
+pod "WaveAnimationView"
+
+end
--- minum/WaterViewController.swift
+++ minum/WaterViewController.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
+import WaveAnimationView
 
 class WaterViewController: UIViewController {
 
+    @IBOutlet weak var lapView: UIView!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    var wave: WaveAnimationView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        progressView.layer.cornerRadius = 15
+        progressView.clipsToBounds = true
+        lapView.layer.cornerRadius = lapView.frame.size.width/2
+        lapView.clipsToBounds = true
+
+        lapView.layer.borderColor = UIColor(rgb: 0x4D80E4).cgColor
+        lapView.layer.backgroundColor = UIColor(rgb: 0x253961).cgColor
+        lapView.layer.borderWidth = 5.0
+    
+    
+        wave = WaveAnimationView(frame: CGRect(origin: .zero, size: lapView.bounds.size), color: UIColor(rgb: 0x5CC2F4))
+               lapView.addSubview(wave)
+               wave.startAnimation()
     }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        wave.stopAnimation()
+        
+    }
+    
 
 
 }
 
+extension UIColor {
+   convenience init(red: Int, green: Int, blue: Int) {
+       assert(red >= 0 && red <= 255, "Invalid red component")
+       assert(green >= 0 && green <= 255, "Invalid green component")
+       assert(blue >= 0 && blue <= 255, "Invalid blue component")
+
+       self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+   }
+
+   convenience init(rgb: Int) {
+       self.init(
+           red: (rgb >> 16) & 0xFF,
+           green: (rgb >> 8) & 0xFF,
+           blue: rgb & 0xFF
+       )
+   }
+}

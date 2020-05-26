7b78e66e87b9f61d9717f2b30738f1487307ae79
Ihwan <mynameisihwan@gmail.com>
2020-05-27 04:45:57 +0700

    create coredata entity & coredata manager

---
+++ minum/history/CoreDataManager.swift
@@ -0,0 +1,98 @@
+//
+//  CoreDataManager.swift
+//  minum
+//
+//  Created by Ihwan ID on 27/05/20.
+//  Copyright © 2020 Ihwan ID. All rights reserved.
+//
+
+import CoreData
+
+struct CoreDataManager {
+
+    static let shared = CoreDataManager()
+
+    let persistentContainer: NSPersistentContainer = {
+
+        let container = NSPersistentContainer(name: "minum")
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error {
+                fatalError("Loading of store failed \(error)")
+            }
+        }
+
+        return container
+    }()
+    
+    @discardableResult
+    func createDrink(date: Date) -> Drink? {
+        let context = persistentContainer.viewContext
+        
+        let drink = NSEntityDescription.insertNewObject(forEntityName: "Drink", into: context) as! Drink // NSManagedObject
+
+        drink.date = date
+
+        do {
+            try context.save()
+            return drink
+        } catch let createError {
+            print("Failed to create: \(createError)")
+        }
+
+        return nil
+    }
+
+    func fetchDrinks() -> [Drink]? {
+        let context = persistentContainer.viewContext
+
+        let fetchRequest = NSFetchRequest<Drink>(entityName: "Drink")
+
+        do {
+            let drinks = try context.fetch(fetchRequest)
+            return drinks
+        } catch let fetchError {
+            print("Failed to fetch companies: \(fetchError)")
+        }
+
+        return nil
+    }
+
+    func fetchDrink(withName name: String) -> Drink? {
+        let context = persistentContainer.viewContext
+
+        let fetchRequest = NSFetchRequest<Drink>(entityName: "Drink")
+        fetchRequest.fetchLimit = 1
+        fetchRequest.predicate = NSPredicate(format: "name == %@", name)
+
+        do {
+            let drinks = try context.fetch(fetchRequest)
+            return drinks.first
+        } catch let fetchError {
+            print("Failed to fetch: \(fetchError)")
+        }
+
+        return nil
+    }
+
+    func updateDrink(drink: Drink) {
+        let context = persistentContainer.viewContext
+
+        do {
+            try context.save()
+        } catch let createError {
+            print("Failed to update: \(createError)")
+        }
+    }
+
+    func deleteDrink(drink: Drink) {
+        let context = persistentContainer.viewContext
+        context.delete(drink)
+
+        do {
+            try context.save()
+        } catch let saveError {
+            print("Failed to delete: \(saveError)")
+        }
+    }
+
+}
--- minum/history/HistoryViewController.swift
+++ minum/history/HistoryViewController.swift
@@ -9,11 +9,51 @@
 import UIKit
 
 class HistoryViewController: UIViewController {
-
+    
+    @IBOutlet weak var historyTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+DemoCoreData()
     }
+    
+    
+    
+}
+
+func DemoCoreData() {
+    // Create
+    guard let newDrink = CoreDataManager.shared.createDrink(date: Date()) else { return }
+    print("Created \(newDrink)")
+
+    // Read
+
+    guard let drinks = CoreDataManager.shared.fetchDrinks() else { return }
+
+    
+
+    print("Number : \(drinks)")
+    print("Number hours: \(String(describing: drinks[1].history?.hours))")
+    print("Number drinks: \(drinks.count)")
+}
+
+extension HistoryViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+}
+
+extension HistoryViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "PlaceCell", for: indexPath)
+                   
+            
+                 
+        return cell
+    }
+    
     
 }

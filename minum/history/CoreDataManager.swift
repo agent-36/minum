7d9ec1e85eabd23d888c41e6d16dc4e72bc042e3
Ihwan <mynameisihwan@gmail.com>
2020-06-02 20:08:13 +0700

    read data from coredata

--- minum/history/CoreDataManager.swift
+++ minum/history/CoreDataManager.swift
@@ -7,6 +7,7 @@
 //
 
 import CoreData
+import UIKit
 
 struct CoreDataManager {
 
@@ -25,16 +26,33 @@ struct CoreDataManager {
     }()
     
     @discardableResult
-    func createDrink(date: Date) -> Drink? {
+    func createDrink(image: UIImage, amount: Int) -> Drink? {
         let context = persistentContainer.viewContext
         
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE, dd LLLL"
+        let current_date = dateFormatter.string(from: date)
+    
+        let idFormatter = DateFormatter()
+        idFormatter.dateFormat =  "yyyyMMdd"
+        let id = dateFormatter.string(from: date)
+        
         let drink = Drink(context: context)
-        drink.date = "Today"
+        drink.id = id
+        drink.date = current_date
         
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+        let minute = calendar.component(.minute, from: date)
+       
+        print("jam \(hour) : \(minute)")
+        
+        let imageData = image.jpegData(compressionQuality: 1.0)
         let history = History(context: context)
-        history.amount = 100
-        history.hours = "17.00"
-        
+        history.amount = Int32(amount)
+        history.hours = "\(hour):\(minute)"
+        history.photo = imageData
         drink.history = NSSet.init(array: [history, history])
 
         do {
--- minum/history/HistoryViewController.swift
+++ minum/history/HistoryViewController.swift
@@ -19,7 +19,7 @@ class HistoryViewController: UIViewController {
         historyTable.dataSource = self
         historyTable.delegate = self
         historyTable.register(UINib(nibName: "HistoryTableViewCell", bundle: nil), forCellReuseIdentifier: "HistoryCell")
-        DemoCoreData()
+    
     }
     
     func applicationDirectoryPath() -> String {
@@ -29,20 +29,9 @@ class HistoryViewController: UIViewController {
 
 
 
-func DemoCoreData() {
-    // Create
-    guard let newDrink = CoreDataManager.shared.createDrink(date: Date().addingTimeInterval(5 * 60 * 24 * 100)) else { return }
-
-    guard let drinks = CoreDataManager.shared.fetchDrinks() else { return }
-
-    guard let histories = CoreDataManager.shared.fetchHistories() else { return }
-    for history in histories{
-        print("jumlah \(history.ofDrink?.date),, \(history.amount)")
-    }
-}
-
 extension HistoryViewController: UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toDetail", sender: self)
     }
 }
 
@@ -51,18 +40,15 @@ extension HistoryViewController: UITableViewDataSource{
         10
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {        let drinks: [Drink] = CoreDataManager.shared.fetchDrinks()!
-//    let dateFormatterPrint = DateFormatter()
-//    dateFormatterPrint.dateFormat = "MMM dd,yyyy"
-//        ///let drink = fetchedResultsController.object(at: indexPath)
-//        let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
-//        cell.textLabel?.text = "\(dateFormatterPrint.string(from: drinks?.last?.date ?? Date()) )"
-//
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let drinks = CoreDataManager.shared.fetchDrinks()
        
         let cell = tableView.dequeueReusableCell(withIdentifier: "HistoryCell", for: indexPath) as! HistoryTableViewCell
-                
-        cell.desc.text = "You drank 1980 ml water"
-        cell.date.text = "today"
+        let arr: [History] = (drinks!.last!.history!.allObjects as? [History])!
+    
+        cell.desc.text = "You drank \(arr.first!.amount) ml water"
+        cell.date.text = "\(drinks?.last?.date ?? "nodata")"
                  
 
         return cell
